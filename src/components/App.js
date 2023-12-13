671022d9e6a0e03fd67195ce7a03127360679d97
Naveen Kumar <nvnkr09@gmail.com>
2023-12-13 17:51:21 +0530

    New changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count, setCount] = useState(0);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-  )
+  );
 }
+
 
 export default App
